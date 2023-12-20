88876b73b9033574b4fdd0491c3b21c530eecf2e
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-12-20 09:59:22 +0900

    feat: 코드 수정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3000;
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
+console.log(
+  `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
